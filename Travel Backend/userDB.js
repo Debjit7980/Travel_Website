77912ea51dd449226e0ccc9844b3c13b6e761e90
Debjit7980 <62738111+Debjit7980@users.noreply.github.com>
2024-01-02 03:48:24 +0530

    Update userDB.js

--- Travel Backend/userDB.js
+++ Travel Backend/userDB.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 const bcrypt=require('bcryptjs');
 const jwt = require('jsonwebtoken');
-mongoose.connect("mongodb+srv://debjitsingharoy007: O2b13SGjjeIUJ4Jg@cluster0.gdcevv7.mongodb.net/?retryWrites=true&w=majority/TravelDb",{useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connect("mongodb+srv://debjitsingharoy007:O2b13SGjjeIUJ4Jg@cluster0.gdcevv7.mongodb.net/?retryWrites=true&w=majority/TravelDb",{useNewUrlParser:true,useUnifiedTopology:true});
 
 const secretKey="debjitsingharoy"
 const users=new mongoose.Schema({
