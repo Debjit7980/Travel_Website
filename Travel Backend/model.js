58d5c422371cac1333d5b80bdab3855f8f4308eb
Debjit7980 <62738111+Debjit7980@users.noreply.github.com>
2024-01-02 03:48:38 +0530

    Update model.js

--- Travel Backend/model.js
+++ Travel Backend/model.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect("mongodb+srv://debjitsingharoy007: O2b13SGjjeIUJ4Jg@cluster0.gdcevv7.mongodb.net/?retryWrites=true&w=majority/TravelDb",{useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connect("mongodb+srv://debjitsingharoy007:O2b13SGjjeIUJ4Jg@cluster0.gdcevv7.mongodb.net/?retryWrites=true&w=majority/TravelDb",{useNewUrlParser:true,useUnifiedTopology:true});
 const treks=new mongoose.Schema({
     name:String,
     imgUrl:String,
