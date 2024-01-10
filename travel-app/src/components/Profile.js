ba084e2aa6049fb8034ea1530e7af6297cb4c83a
Debjit7980 <62738111+Debjit7980@users.noreply.github.com>
2024-01-11 03:38:05 +0530

    Update Profile.js

--- travel-app/src/components/Profile.js
+++ travel-app/src/components/Profile.js
@@ -124,7 +124,7 @@ function Profile() {
     const email = loginData.validUserOne ? loginData.validUserOne.email : '';
     const pp = loginData.validUserOne ? loginData.validUserOne.profilePicture : '';
     const _id = loginData.validUserOne ? loginData.validUserOne._id : ' ';
-    const trimmedPath = pp?pp.replace(/.*public(\\|\/)/, '/'): ' ';
+     const trimmedPath = pp?pp.replace(/.*opt(\\|\/)src(\\|\/)/, '/'): ' ';
     console.log(trimmedPath);
 
     return (
